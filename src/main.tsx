57bc5b6ee1e2d07f97d453c78586bcd7e830dee6
zahra <z.godarzi75@gmail.com>
2026-02-25 10:33:23 +0330

    feat:add persist

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,19 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.tsx";
 import "./index.css";
-import { store } from "./store/store.ts";
+import { persistor, store } from "./store/store.ts";
 
 const queryclient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<QueryClientProvider client={queryclient}>
 			<Provider store={store}>
-				<App />
+				<PersistGate loading={null} persistor={persistor}>
+					<App />
+				</PersistGate>
 			</Provider>
 		</QueryClientProvider>
 	</StrictMode>,
