176b1fba69d6b26198a61263abb16f32d0406ddf
zahra <z.godarzi75@gmail.com>
2026-02-25 10:16:07 +0330

    feat:add searchUser

--- src/components/user-List/userList.tsx
+++ src/components/user-List/userList.tsx
@@ -1,9 +1,19 @@
-import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import {
+	Card,
+	CardContent,
+	CardMedia,
+	Container,
+	Grid,
+	TextField,
+	Typography,
+} from "@mui/material";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useUsers } from "../../hooks/useUsers";
 import { setSelectedUser } from "../../store/slice/taskSlice";
 
 export default function UserList() {
+	const [search, setSearch] = useState("");
 	const dispatch = useDispatch();
 	const { data, isLoading, error } = useUsers();
 	if (isLoading) {
@@ -20,35 +30,50 @@ export default function UserList() {
 			</div>
 		);
 	}
+	const filteredUsers = data?.filter((user) =>
+		user.name.toLowerCase().includes(search.toLowerCase()),
+	);
+
 	return (
-		<Grid
-			container
-			spacing={{ xs: 2, md: 3 }}
-			columns={{ xs: 4, sm: 8, md: 12 }}
-		>
-			{data?.map((user) => (
-				<Grid key={user.id} size={{ xs: 2, sm: 4, md: 4 }}>
-					<Card
-						sx={{ maxWidth: 345 }}
-						onClick={() => dispatch(setSelectedUser(user.id))}
-					>
-						<CardMedia
-							component="img"
-							alt={user.name}
-							height="140"
-							image={user.avatar}
-						/>
-						<CardContent>
-							<Typography gutterBottom variant="h5" component="div">
-								{user.name}
-							</Typography>
-							<Typography variant="body2" sx={{ color: "text.secondary" }}>
-								{user.email}
-							</Typography>
-						</CardContent>
-					</Card>
-				</Grid>
-			))}
-		</Grid>
+		<Container>
+			<TextField
+				fullWidth
+				label="Search user..."
+				variant="outlined"
+				margin="normal"
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+			/>
+
+			<Grid
+				container
+				spacing={{ xs: 2, md: 3 }}
+				columns={{ xs: 4, sm: 8, md: 12 }}
+			>
+				{filteredUsers?.map((user) => (
+					<Grid key={user.id} size={{ xs: 2, sm: 4, md: 4 }}>
+						<Card
+							sx={{ maxWidth: 345, cursor: "pointer" }}
+							onClick={() => dispatch(setSelectedUser(user.id))}
+						>
+							<CardMedia
+								component="img"
+								alt={user.name}
+								height="140"
+								image={user.avatar}
+							/>
+							<CardContent>
+								<Typography gutterBottom variant="h5">
+									{user.name}
+								</Typography>
+								<Typography variant="body2" sx={{ color: "text.secondary" }}>
+									{user.email}
+								</Typography>
+							</CardContent>
+						</Card>
+					</Grid>
+				))}
+			</Grid>
+		</Container>
 	);
 }
